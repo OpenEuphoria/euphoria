8a1f5009ab48559715c771a6e9826f25c93e0c96
Rob Craig <rds@rapideuphoria.com>
2007-06-26 23:53:32 +0000

    added double-quotes around filename for Esc e command

--- bin/FreeBSD/ed.ex
+++ bin/FreeBSD/ed.ex
@@ -1,3 +1,6 @@
+-- special version for FreeBSD
+-- should try to merge with standard ed.ex for DOS/WIN/Linux
+
 	----------------------------------------------------------
 	--       This Euphoria Editor was developed by          --
 	--            Rapid Deployment Software.                --
@@ -1755,7 +1758,7 @@ procedure get_escape(boolean help)
 	    end if
 	end if
 	if platform() = LINUX then
-	    shell("exu " & file_name)
+	    shell("exu " & "\"" & file_name & "\"")
 	elsif match(".exw", lower(file_name)) or 
 	      match(".ew",  lower(file_name)) then
 	    shell("exw " & file_name)
