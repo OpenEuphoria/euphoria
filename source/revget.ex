6640962840720ec514fa3b75a2486ea4d1e43378
Jim C. Brown <jimcbrown@openeuphoria.org>
2008-06-10 00:12:20 +0000

    Don't rely on system() for revget.ex because some systems are broken

--- source/revget.ex
+++ source/revget.ex
@@ -1,4 +1,86 @@
+include get.e 
 constant rev = "revision=\""
+
+function is_numeric(sequence s)
+	for i = 1 to length(s) do
+		if atom(s[i]) then
+			if not find(s[i], "0123456789") then
+				return 0
+			end if
+		else
+			return 0
+		end if
+	end for
+	return 1
+end function
+
+procedure rev_1_4()
+
+sequence c
+integer h
+sequence f
+object x
+sequence g
+integer n
+
+c = command_line()
+if length(c) != 3 then
+	puts(2, "Incorrect usage.\n")
+	abort(1)
+end if
+
+h = open(c[3], "r")
+if h = -1 then
+	puts(2, "Unable to open.\n")
+	abort(1)
+end if
+
+x = gets(h)
+f = ""
+while not atom(x) do
+	f &= {x}
+	x = gets(h)
+end while
+close(h)
+
+g = {}
+for i = 1 to length(f) do
+	while length(f[i]) and f[i][length(f[i])] = '\n' do
+		f[i] = f[i][1..length(f[i])-1]
+	end while
+	while length(f[i]) and f[i][length(f[i])] = '\r' do
+		f[i] = f[i][1..length(f[i])-1]
+	end while
+	--if length(f[i]) = 3 then
+	if is_numeric(f[i]) then
+		g &= {f[i]}
+	end if
+end for
+
+if not length(g) then
+	? -1
+	abort(0)
+end if
+
+f = g
+for i = 1 to length(g) do
+	f[i] = value(g[i])
+	f[i] = f[i][2]
+end for
+
+n = f[1]
+for i = 2 to length(f) do
+	if f[i] > n then
+		n = f[i]
+	end if
+end for
+
+h = open("rev.e", "w")
+printf(h, "global constant SVN_REVISION = \"%d\"\n", {n})
+close(h)
+end procedure
+
+procedure rev_1_3()
 sequence c
 integer h
 sequence f
@@ -36,9 +118,8 @@ end for
 if not length(f) or not atom(f[1]) then
 	-- newer subversion 1.4 client wc isn't in xml format
 	-- use different parser to guess
-	c[2] = "revget4.ex"
-	system("\""&c[1]&"\" \""&c[2]&"\" \""&c[3]&"\"", 2)
-	abort(0)
+	rev_1_4()
+	return
 end if
 
 f = f[1..find('"', f)-1]
@@ -47,4 +128,6 @@ f = f[1..find('"', f)-1]
 h = open("rev.e", "w")
 printf(h, "global constant SVN_REVISION = \"%s\"\n", {f})
 close(h)
+end procedure
 
+rev_1_3()
