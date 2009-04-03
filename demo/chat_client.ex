63f6168c2525768bf650a8970044d11b8b50fe09
Jeremy Cowgar <jeremy@cowgar.com>
2009-04-03 03:36:50 +0000

    * Organized the code a bit better to make a more clear example. * Now checks for a server disconnect and lets the user know then   terminates the application. * Changes to select() have now been tested on SunOS as well as   Windows.

--- demo/chat_client.ex
+++ demo/chat_client.ex
@@ -4,64 +4,78 @@ include std/console.e
 include std/text.e
 include std/socket.e as slib
 
-puts(1, #/
-____1. You must first enter your nick name.
-	2. When you want to say something, press the "s" key for speak.
-	3. When you want to leave, press the "l" key for leave.
-/)
-
-object name = trim(prompt_string("Enter your nickname please: "))
-
-slib:socket sock = slib:create(slib:AF_INET, slib:SOCK_STREAM, 0)
-
-sequence addr = "127.0.0.1:5000",
-	args = command_line()
-
-if length(args) > 2 then
-	addr = args[3]
-end if
-
-printf(1, "Connecting to %s\n", { addr })
-
-integer result = slib:connect(sock, "127.0.0.1:5000")
-if not result then
-	printf(1, "Could not connect to server %s, is it running?\nError = %d\n", { addr, result })
-	puts(1, "Maybe try connecting to a different IP/Port?\n")
-	puts(1, "   Usage: exwc chat_client.ex [IP:PORT]\n")
+procedure lost_server()
+	puts(1, "We have been disconnected from the server,\n")
+	puts(1, "Application is terminating.\n")
 	abort(1)
-end if
+end procedure
 
--- Send our nickname to the server
-_ = slib:send(sock, name & "\n", 0)
+function get_nick()
+	puts(1, #/
+________1. You must first enter your nick name.
+	    2. When you want to say something, press the "s" key for speak.
+	    3. When you want to leave, press the "l" key for leave.
 
--- Print the server greeting message
-puts(1, recv(sock, 0))
+        /)
+	return trim(prompt_string("Enter your nickname please: "))
+end function
 
-while 1 label "top" do
-	integer key = get_key()
-	switch key do
-		case -1 then
-			break
+procedure main(sequence args)
+	slib:socket sock = slib:create(slib:AF_INET, slib:SOCK_STREAM, 0)
+	sequence addr = "127.0.0.1:5000"
 
-		case 's' then
-			sequence data = prompt_string(sprintf("%10s > ", { name }))
-			if length(data) > 0 then
-				_ = send(sock, sprintf("%10s > %s\n", { name, data }), 0)
-			end if
-			break
-
-		case 'l' then
-			exit "top"
-	end switch
-
-	object sock_data = slib:select(sock, 0)
-	if sock_data[1][SELECT_IS_ERROR] then
-		puts(1, "We've lost the server, exiting\n")
-		exit "top"
-
-	elsif sock_data[1][SELECT_IS_READABLE] then
-		puts(1, recv(sock, 0))
+	if length(args) > 2 then
+		addr = args[3]
 	end if
-end while
 
-_ = slib:close(sock)
+	printf(1, "Connecting to %s\n", { addr })
+
+	integer result = slib:connect(sock, "127.0.0.1:5000")
+	if not result then
+		printf(1, "Could not connect to server %s, is it running?\nError = %d\n", { addr, result })
+		puts(1, "Maybe try connecting to a different IP/Port?\n")
+		puts(1, "   Usage: exwc chat_client.ex [IP:PORT]\n")
+		abort(1)
+	end if
+
+	sequence name = get_nick()
+	_ = slib:send(sock, name & "\n", 0)
+
+	-- Print the server greeting message
+	puts(1, recv(sock, 0))
+
+	while 1 label "top" do
+		integer key = get_key()
+		switch key do
+			case -1 then
+				break
+
+			case 's' then
+				sequence data = prompt_string(sprintf("%10s > ", { name }))
+				if length(data) > 0 then
+					_ = send(sock, sprintf("%10s > %s\n", { name, data }), 0)
+				end if
+
+			case 'l' then
+				exit "top"
+		end switch
+
+		object sock_data = slib:select(sock, 0)
+		if sock_data[1][SELECT_IS_ERROR] then
+			lost_server()
+
+		elsif sock_data[1][SELECT_IS_READABLE] then
+			object o = recv(sock, 0)
+			if atom(o) then
+				lost_server()
+			end if
+
+			puts(1, o)
+		end if
+	end while
+
+	_ = slib:close(sock)
+end procedure
+
+main(command_line())
+
