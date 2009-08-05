1208ad95ed8b9e4260309a32245ee4994729d104
Jeremy Cowgar <jeremy@cowgar.com>
2009-08-05 19:58:41 +0000

    * Added .bat file to generate sources on windows so that the   line extensions of all files are windows. * Modified to use the svn export command, thus no need to remove   .svn directories

--- packaging/source/build.sh
+++ packaging/source/build.sh
@@ -11,10 +11,6 @@ fi
 
 rm -f euphoria-$1.tar.gz
 rm -rf cleanbranch
-svn co https://rapideuphoria.svn.sourceforge.net/svnroot/rapideuphoria/$1 cleanbranch
-
-find cleanbranch -name .svn -exec rm -rf {} \;
-
-mv cleanbranch euphoria-src-$1
+svn export https://rapideuphoria.svn.sourceforge.net/svnroot/rapideuphoria/$1 euphoria-src-$1
 
 tar czf euphoria-src-$1.tar.gz euphoria-src-$1
