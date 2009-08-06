abbc161627a6a24499d09d629ce64bad0fea4762
Jeremy Cowgar <jeremy@cowgar.com>
2009-08-06 02:24:39 +0000

    * Updated datesub.ex example and config to work with optional   parameters given by command line or by DLL interface.

--- demo/preproc/datesub.ex
+++ demo/preproc/datesub.ex
@@ -11,7 +11,7 @@ sequence cmdopts = {
 
 public function preprocess(sequence inFileName, sequence outFileName,
         sequence options={})
-	map opts = cmd_parse(cmdopts, options)
+	map opts = cmd_parse(cmdopts, {}, {0,0} & parse_commandline(options))
     sequence content = read_file(inFileName)
 
     content = find_replace("@DATE@", content, format(now(), map:get(opts, "f")))
@@ -22,12 +22,12 @@ public function preprocess(sequence inFileName, sequence outFileName,
 end function
 
 ifdef not EUC_DLL then
-	cmdopts = {
+	sequence c = {
         { "i", 0, "Input filename", { NO_CASE, MANDATORY, HAS_PARAMETER, "filename"} },
         { "o", 0, "Output filename", { NO_CASE, MANDATORY, HAS_PARAMETER, "filename"} }
 	} & cmdopts
 	
-    map opts = cmd_parse(cmdopts)
+    map opts = cmd_parse(c)
     preprocess(map:get(opts, "i"), map:get(opts, "o"),
  		"-f " & map:get(opts, "f", "%Y-%m-%d"))
 end ifdef
