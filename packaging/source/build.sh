fec966a5e57bb8423840350d744b648481994677
Jeremy Cowgar <jeremy@cowgar.com>
2009-08-14 17:53:30 +0000

    * Fixed bindu naming * Changed all $1 $2 $3 to simply be $@ on unix wrapper scripts. There were a few   "copy/paste" bugs found, i.e. $1 $2 $3 %4 %5 %6 ...

--- packaging/source/build.sh
+++ packaging/source/build.sh
@@ -12,4 +12,11 @@ fi
 rm -f euphoria-$1.tar.gz
 svn export https://rapideuphoria.svn.sourceforge.net/svnroot/rapideuphoria/tags/$1 euphoria-src-$1
 
+cd euphoria-$1/bin
+rm -f *.bat *.ico make31.exw *.exe
+chmod ug+rwx,o+rx Linux/*
+cp Linux/* .
+rm -rf Linux FreeBSD
+cd ../
+
 tar czf euphoria-src-$1.tar.gz euphoria-src-$1
