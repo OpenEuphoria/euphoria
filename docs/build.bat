b03885ff9c5b7dd3b9c850efeb0ac9df33ec9d2f
Jeremy Cowgar <jeremy@cowgar.com>
2009-08-29 12:39:16 +0000

    * Added the new net/url.e to the manual assembly file

--- docs/build.bat
+++ docs/build.bat
@@ -2,3 +2,5 @@
 
 ..\bin\eui ..\source\eudoc\eudoc.ex  -v -a manual.af -o euphoria.txt
 ..\bin\eui ..\source\eudoc\creole\creolehtml.ex -A=ON -t=template.html -ohtml euphoria.txt
+rem ..\bin\eudoc  -v -a manual.af -o euphoria.txt
+rem ..\bin\creolehtml -A=ON -t=template.html -ohtml euphoria.txt
