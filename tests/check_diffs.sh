78bfee5742cc87240b924dda940b0de0168c0cfb
Matt Lewis <matthewwalkerlewis@gmail.com>
2010-02-27 01:15:49 +0000

    * fixed check_diffs.sh so it works

--- tests/check_diffs.sh
+++ tests/check_diffs.sh
@@ -6,20 +6,16 @@
 # will implement SAFE behavior for std/machine.e if not memory.e will implement faster behavior
 # for std/machine.e.
 #
-egrep 'export ' ../include/std/safe.e | sort | awk -F '(' '{ print $1; }' | awk -F '=' '{ print $1;
-}' > exported_from_safe.txt 
-egrep 'export ' ../include/std/memory.e | sort | awk -F '(' '{ print $1; }' | awk -F '=' '{ print $1;
-}' > exported_from_memory.txt
+egrep 'export ' ../include/std/safe.e | sort | awk -F '(' '{ print $1; }' | awk -F '=' '{ print $1;}' > exported_from_safe.txt 
+egrep 'export ' ../include/std/memory.e | sort | awk -F '(' '{ print $1; }' | awk -F '=' '{ print $1;}' > exported_from_memory.txt
 if diff exported_from_safe.txt exported_from_memory.txt; then
 	echo "Both std/memory.e and std/safe.e provide the same interface to std/machine.e....good." 
 else
 	echo "Error: std/memory.e and std/safe.e export different symbols to std/machine.e....bad."
 	exit 1
 fi
-egrep 'public ' ../include/std/safe.e | sort | awk -F '(' '{ print $1; }' | awk -F '=' '{ print $1;
-}' > publicized_from_safe.txt 
-egrep 'public ' ../include/std/memory.e | sort | awk -F '(' '{ print $1; }' | awk -F '=' '{ print $1;
-}' > publicized_from_memory.txt
+egrep 'public ' ../include/std/safe.e | sort | awk -F '(' '{ print $1; }' | awk -F '=' '{ print $1;}' > publicized_from_safe.txt 
+egrep 'public ' ../include/std/memory.e | sort | awk -F '(' '{ print $1; }' | awk -F '=' '{ print $1;}' > publicized_from_memory.txt
 if diff publicized_from_safe.txt publicized_from_memory.txt | grep '>'; then
 	echo "Error: std/memory.e symbols not in std/safe.e to all files        ...bad."
 	exit 1
