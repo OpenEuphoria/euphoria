19d9ae1f8052fd3dc7094c24fb9d478df46d08a3
Derek Parnell <ddparnell@bigpond.com>
2010-03-30 15:57:20 +0000

    * Documented the file formats for the ECP code page text files, and the code page database.

--- bin/buildcpdb.ex
+++ bin/buildcpdb.ex
@@ -9,7 +9,59 @@ include std/convert.e
 include std/search.e
 include std/sort.e
 include std/datetime.e
+include std/map.e
 
+-- ** Format of the codepage database **
+-- The database is a non-text file. It is divided into a number of sections:
+--
+-- IDENTITY section.
+-- This contains a single serialized sequence, which contains ...
+--  ECP Databse format version. This is an integer.
+--  Creation date (UTC) string in the for YYYYMMDDhhmmss.
+--  The version of Euphoria that this format supports. A sequence of 4 integers
+--                              {Major, Minor, Revision, Build}
+--  The creation tool identity. A two-element sequence, {tool name, too version}
+--
+-- NAMES OFFSET section
+-- This is a single serialized value. It contains the file offset to the NAMES
+-- section.
+--
+-- CODE PAGE section
+-- This contains six sets of serialized values per code page.
+--  The list of uppercase characters in this code page.
+--  The list of lowercase characters in this code page.
+--  The code page's title
+--  The code page's Bytes per Character value
+--  The list of character values in the code page.
+--  The list of equivalent unicode values for the characters.
+--    
+-- UNICODE section
+-- This contains two serialized values. The first is a sequence containing all
+-- the unicode code point values used in the codes pages stored in the database.
+-- And the second is a sequence of corresponding names for those code points.
+--
+-- NAMES section
+-- This contains a serialized sequence containing two elements. The first
+-- is a set of strings, the name of each code page stored in the database
+-- plus the name 'unicode'. The second is a set of corresponding database offsets
+-- that point to the code page's section. The 'unicode' offset points to the
+-- UNICODE section.
+--
+--
+-- The method to use in reading the database is ...
+-- * Read in the IDENTITY section.
+-- * Read in the NAMES OFFSET section.
+-- * Use the offset in the NAMES OFFSET section to position the database to the
+--   NAMES section.
+-- * Read in the NAMES section.
+-- * Scan the name strings for the code page you are looking for. When found,
+--     use the respective database offset to reposition the data base to the 
+--     code page's section.
+-- * Read in the code page data. This is set of six serialized values to read in. 
+-- * If you want the database's unicode values, scan the name strings for 'unicode'. Use
+--      its respective offset value to reposition the database to the UNICODE
+--      section and then read on that section.
+ 
 constant current_version = {1,0,0}
 
 object void
@@ -22,7 +74,7 @@ sequence current_db_name
 integer silent_running = 0
 
 object v_ECP_List
-sequence unicode_list = {}
+object unicode_list = map:new()
 
 sequence name_list = {}
 sequence posn_list = {}
@@ -99,7 +151,7 @@ if db = -1 then
 end if
 
 puts(db, serialize({1, -- ECP Databse format version
-          format(now_gmt(), "%Y%m%d%H%M%S" ), -- date
+          datetime:format(now_gmt(), "%Y%m%d%H%M%S" ), -- date
           {4,0,0,0}, -- eu:version()
           {filebase(cmdline[2]), current_version } -- creation tool identity
          })
@@ -119,9 +171,9 @@ name_list = append(name_list, "unicode")
 posn_list &= 0
 posn_list[$] = where(db)
 
-unicode_list = sort(unicode_list)
-puts(db, serialize( vslice(unicode_list, 1) ) ) -- "code points"
-puts(db, serialize( vslice(unicode_list, 2) ) ) -- "names "
+sequence unicode_used = map:pairs(unicode_list, 1)
+puts(db, serialize( vslice(unicode_used, 1) ) ) -- "code points"
+puts(db, serialize( vslice(unicode_used, 2) ) ) -- "names "
 
 
 integer indx_pos = where(db)
@@ -153,6 +205,44 @@ elsif not silent_running then
 end if
 void = delete_file(backup_db_name)
 	
+-- ** The format for the Euphoria Code Page file (ecp) **  
+-- Each file is a text file containing a number of sections.
+-- A section begins with a line starting with "--<name>--" where <name>
+-- the the section name. Currently three types of sections are supported:
+-- HEAD section.
+-- This contains information about the file. It has two lines:
+--  'Title = <text>' and 'BPC = <value>' where <text> is the displayable
+-- title for the code page, and <value> is the number of bytes per character
+-- used for each character value. If BPC is omitted, 1 is assumed.
+-- eg.
+--
+--  --HEAD--
+--  Title = OEM 437 (US)
+--  BPC = 1
+--
+-- CODE section.
+-- This contains one line per character in the code page. Each line takes the
+-- format of ...
+--    hexcode = Unicode : Name
+-- where 'hexcode' is the hexadecimal value of the character,
+--       'Unicode' is the equivalent unicode value is the form 'U+xxxx' where
+--                 'xxxx' is the hexadecimal value of the unicode code point.
+--       'Name' is a text description of the character.
+-- eg.
+--
+--   60 = U+0060 : GRAVE ACCENT
+--   61 = U+0061 : LATIN SMALL LETTER A
+--
+-- CASE section.
+-- This contains the upper to lower case conversion table. It has one line
+-- for each upper case character that has a equivalent lower case character
+-- in the same code page. Each line has the format "uu = ll" where 'uu' is
+-- the uppercase hexadecimal character value and 'll' is the hexadecimal
+-- lowercase character value.
+-- eg.
+--    41 = 61
+--    42 = 62
+--
 
 procedure load_file(sequence file_path)
 	integer pos
@@ -164,8 +254,8 @@ procedure load_file(sequence file_path)
 	sequence value
 	integer key_num
 	integer unicode
-	sequence vl_title = {}
-	sequence vl_bpc = {}
+	sequence vl_title = ""
+	sequence vl_bpc = "1"
 	sequence vl_codes = {}
 	sequence vl_unicodes = {}
 	sequence vl_uppercase = {}
@@ -220,6 +310,7 @@ procedure load_file(sequence file_path)
 		end if
 		
 	end while
+	
 	-- Process 'code' section
 	while pos < length(file_text) do
 		pos += 1
@@ -250,15 +341,8 @@ procedure load_file(sequence file_path)
 		value = trim(kv[2])
 		vl_codes &= key_num
 		vl_unicodes &= unicode
-		for i = 1 to length(unicode_list) do
-			if unicode = unicode_list[i][1] then
-				unicode = -1
-				exit
-			end if
-		end for
-		if unicode >= 0 then
-			unicode_list = append(unicode_list, {unicode, value})
-		end if
+		
+		map:put(unicode_list, unicode, value, map:LEAVE)
 				
 		
 	end while
