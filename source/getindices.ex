4502a749f6d096b4f8a6bce180dec04f7cd3f896
Matt Lewis <matthewwalkerlewis@gmail.com>
2010-10-04 19:08:57 +0000

    * re-fix getindices.ex

--- source/getindices.ex
+++ source/getindices.ex
@@ -6,7 +6,6 @@ include std/map.e  as map
 include std/sequence.e as seq
 include std/console.e as con
 include std/io.e as io
-include std/error.e as error
 
 function extract_slices( sequence s, sequence indicies )
 	sequence out
@@ -18,20 +17,15 @@ function extract_slices( sequence s, sequence indicies )
 end function
 
 -- Example:
---<a href="eu400_0101.html#_5768_map_type">13.6.7.38 MAP_TYPE</a><br />
---<a href="(?<file>eu400_0101.html)#(?P<bookmark>_5768_map_type)">(?P<chatper>13.6.7.38) (?P<clean_id>MAP_TYPE)</a><br />
-	
-constant id_pattern = re:new( `<a href="(?P<file>eu400_\d+.html)#`&
-	`(?P<bookmark>_\d+_[a-zA-Z_:]+)">`&
-	`(?P<chapter>[0-9.]+) `&
-	`(?P<clean_id>[a-z0-9A-Z_ -]+)</a>`, {ANCHORED,EXTRA} )
+-- <a href="eu400_0102.html#_5747_map_type">MAP_TYPE (Memory Management - Low-Level)</a>
+sequence id_pattern = re:new( `<a href="([a-z_0-9]+\.html)#(_\d+_[A-Za-z_0-9]+)">([a-xA-Z0-9_]+) \(([a-z_0-9A-Z -]+)\)`, EXTRA )
 
-enum 
+enum
 	ENTIRE_MATCH = 1,
 	FILE,
 	BOOKMARK,
-	CHAPTER,
-	CLEAN_ID 
+	CLEAN_ID,
+	CHAPTER
 
 sequence id, url, /*section,*/ chapter
 map:map dictionary
@@ -58,63 +52,51 @@ end function
 
 sequence cl = command_line()
 htmlfd = open_or_die(cl[$-1],"r")
-ifdef DEBUG then
-    jsfd = 1
-elsedef
-    jsfd   = open_or_die(cl[$],"w")
-    printf(jsfd,"index=new Array();\nchapter=new Array();",{})
-end ifdef
+jsfd   = open_or_die(cl[$],"w")
 templfd = open_or_die(`../docs/search-template.js`,"r")
 line = gets(htmlfd)
 count = 0
 dictionary = map:new(3000)
+printf(jsfd,"index=new Array();\nchapter=new Array();",{})
 while sequence(line) do
     match_data = re:matches( id_pattern, line )
     if sequence(match_data) then
 		printf(jsfd,"// %s",{line})
-		ifdef DEBUG then
-		    count += 1
-		elsedef
-		    id = match_data[CLEAN_ID]	
-		    url = match_data[FILE] & '#' &
-			    match_data[BOOKMARK]
-		    chapter = match_data[CHAPTER]
-		    printf(jsfd,`chapter['%s']='%s';`,
-		    {chapter,id})
-		    if not eu:find(' ',id) then
-			    if not map:has(dictionary,id) then
-				    printf(jsfd,`index['%s'] = new Array();%s`, {id,"\n"} )
-				    map:put(dictionary,id,0)
-			    else
-				    map:put(dictionary,id,1,ADD)
-			    end if
-			    printf(jsfd,`
-_____________________________t = new Array();
-			     t.url = '%s';
-			     t.chapter = '%s';
-			     index['%s'][%d] = t;`
-			     , 
-				    {url, chapter, id, map:get(dictionary,id), url } 
-			    )
-			    count += 1
-		    end if
-		end ifdef
+		id = match_data[CLEAN_ID]	
+		url = match_data[FILE] & '#' &
+			match_data[BOOKMARK]
+		chapter = match_data[CHAPTER]
+		printf(jsfd,`chapter['%s']='%s';`,
+		{chapter,id})
+		if not eu:find(' ',id) then
+			if not map:has(dictionary,id) then
+				printf(jsfd,`index['%s'] = new Array();%s`, {id,"\n"} )
+				map:put(dictionary,id,0)
+			else
+				map:put(dictionary,id,1,ADD)
+			end if
+			printf(jsfd,
+			`t = new Array();
+			t.url = '%s';
+			t.chapter = '%s';
+			index['%s'][%d] = t;
+			`, 
+				{url, chapter, id, map:get(dictionary,id), url } 
+			)
+			count += 1
+		end if
     end if
     line = gets(htmlfd)
 end while
 close(htmlfd)
 
-ifdef not DEBUG then
-    -- copy script code to the end of the generated script.
-    line = gets(templfd)
-    while sequence(line) do
-	    puts(jsfd,line)
-	    line = gets(templfd)
-    end while
-    close(templfd)
-    close(jsfd)
-end ifdef
+line = gets(templfd)
+while sequence(line) do
+	puts(jsfd,line)
+	line = gets(templfd)
+end while
+close(templfd)
+close(jsfd)
+
 printf(1, "Matched %d lines\n", { count } )
-if count = 0 then
-	crash( "index.html format has changed." )
-end if
+
