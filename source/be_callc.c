cba3d232ae93cf40c9364cd131016b8f4bfb085e
Jim C. Brown <jimcbrown@openeuphoria.org>
2010-11-30 19:43:01 +0000

    * Apply previous fix in MinGW code

--- source/be_callc.c
+++ source/be_callc.c
@@ -691,7 +691,7 @@ object call_c(int func, object proc_ad, object arg_list)
 
 	// Setup and Check for Errors
 	proc_index = get_pos_int("c_proc/c_func", proc_ad); 
-	if ( proc_index >= c_routine_next) {
+	if (proc_index >= (unsigned)c_routine_next) {
 		sprintf(TempBuff, "c_proc/c_func: bad routine number (%ld)", proc_index);
 		RTFatal(TempBuff);
 	}
