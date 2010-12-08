6aedf0d609b3eaf1da98aa988fec8eca724b01d0
Jeremy Cowgar <jeremy@cowgar.com>
2010-12-08 15:44:43 +0000

    Updated *nix build.sh for new utilities and distribution layout

--- packaging/unix/build.sh
+++ packaging/unix/build.sh
@@ -5,16 +5,23 @@
 #
 
 if [ "$1" = "" ] || [ "$2" = "" ]; then
-	echo Usage: build.sh PLATFORM TAG-NAME
+	echo Usage: build.sh SVN_PATH VERSION
         exit
 fi
 
-PLATFORM=$1
-TAG=$2
-REL_NAME=euphoria-${TAG}-${PLATFORM}
+SVN_PATH=$1
+VERSION=$2
+OPT=$3
+REL_NAME=euphoria-${VERSION}${OPT}
+ROOTDIR=../../..
 
-svn export https://rapideuphoria.svn.sourceforge.net/svnroot/rapideuphoria/tags/${TAG} ${REL_NAME}
+echo Cleaning previous working directory
+rm -rf ${REL_NAME}
 
+echo Getting a clean SVN export
+svn export https://rapideuphoria.svn.sourceforge.net/svnroot/rapideuphoria/${SVN_PATH} ${REL_NAME}
+
+echo Removing unnecessary file for *nix installs
 cd ${REL_NAME}/bin
 rm -f *.bat *.ico make31.exw *.exe
 cd ..
@@ -22,14 +29,28 @@ cd ..
 rm -rf docs
 rm -rf packaging
 
-cp ../../../bin/eu{i,b,c,doc,test,coverage,bind} ../../../bin/eu.a ../../../bin/eudbg.a bin
-cp ../../../bin/creolehtml ../../../bin/ecp.dat bin
+echo Stripping binaries that will be installed
+BINS=`ls ${ROOTDIR}/bin/eu{b,bind,c,i,shroud} ${ROOTDIR}/bin/eu{coverage,dis,dist,doc,loc,test} ${ROOTDIR}/bin/creolehtml`
+for f in ${BINS}; do
+	echo Stripping/copying ${f}
+	strip ${f}
+	cp ${f} bin
+done
+
+echo Copying compiled executables to our distribution directory
+cp ${ROOTDIR}/bin/ecp.dat ${ROOTDIR}/bin/eu{.a,dbg.a} bin
+
+echo Copying docs to our distribution directory
 
 mkdir docs
-cp ../../../build/*.pdf docs
-cp -r ../../../build/html docs
+cp ${ROOTDIR}/build/*.pdf docs
 
 cd ..
 
+echo Creating distribution tar.gz
 tar czf ${REL_NAME}.tar.gz ${REL_NAME}
 
+echo Creating distribution tar.bz2
+tar cjf ${REL_NAME}.tar.bz2 ${REL_NAME}
+
+echo Done!
