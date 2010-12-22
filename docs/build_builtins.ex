527a8eb7a520ba6cb890a272dd4595ba4c632225
Jeremy Cowgar <jeremy@cowgar.com>
2010-12-22 01:17:48 -0500

    Changed the built-in list to be a 4 column table instead of a single list.

--- docs/build_builtins.ex
+++ docs/build_builtins.ex
@@ -13,7 +13,18 @@ These methods are built into Euphoria and require no includes.
 
 integer x = 1
 for i = 1 to length(builtins) do
-	printf(fh, "* [[:%s]]\n", { builtins[i] })
+	printf(fh, "| ##[[:%s]]## ", { builtins[i] })
+
+	if x = 4 then
+		printf(fh, "|\n")
+		x = 1
+	else
+		x += 1
+	end if
+end for
+
+for i = x - 1 to 4 do
+	printf(fh, " | ")
 end for
 
 printf(fh, "\n")
