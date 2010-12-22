21da8816e5a9f869a97aa9649e9ee345574dcb3b
Jim C. Brown <jimcbrown@openeuphoria.org>
2010-12-22 14:17:07 -0500

    * More sync cleanup, get rid of files that were deleted in default branch

--- docs/build.bat
+++
@@ -1,6 +0,0 @@
-@ echo off
-
-..\bin\eui ..\source\eudoc\eudoc.ex  -v -a manual.af -o euphoria.txt
-..\bin\eui ..\source\eudoc\creole\creolehtml.ex -A=ON -t=template.html -ohtml euphoria.txt
-rem ..\bin\eudoc  -v -a manual.af -o euphoria.txt
-rem ..\bin\creolehtml -A=ON -t=template.html -ohtml euphoria.txt
