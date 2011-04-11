04cc38d33b6371819d66258a465c0bc48b930bd5
Shawn Pringle <shawn.pringle@gmail.com>
2011-04-11 14:14:15 -0300

    * added more to inner_trials to lower the standard deviation * moved display and data processing to a routine display_report().

--- demo/bench/math_ops.ex
+++ demo/bench/math_ops.ex
@@ -4,8 +4,9 @@ include std/console.e
 
 sequence s1 = rand(repeat(255,5000))
 sequence s2 = rand(repeat(255,5000))
+sequence save = {s1,s2}
 sequence ans
-constant inner_trials = 10_000
+constant inner_trials = 50_000
 
 constant neg_prefixes = { {0.001,"m"} , {0.00_000_1,"mc"}, {0.00_000_000_1,"n"}, {0.00_000_000_000_1,"p"} }
 function get_metric_prefix( atom value )
@@ -20,7 +21,7 @@ function get_metric_prefix( atom value )
 end function
 
 -- we need large trial counts to get the standard deviation down...
-function sum_trial_2()
+function sum_trial_is()
 	atom ti 
 	ti = time()
 	while ti = time() do
@@ -33,16 +34,25 @@ function sum_trial_2()
 	return (tf-ti)/inner_trials
 end function
 
-sequence timing_data = {}
-for i = 1 to 20 do
-	timing_data = append(timing_data, sum_trial_2())
-end for
-sequence stats = average(timing_data) & 2*stdev(timing_data)
-sequence prefix = get_metric_prefix(stats[1])
-stats /= prefix[1]
-printf(1, "Time to add an integer to a 5000 long sequence of integers: %.3f +- %f %ss\n", append(stats,prefix[2]) )
+procedure display_report(sequence message, sequence routine_name)
+	integer r = routine_id(routine_name)	
+	sequence timing_data = {}
+	for i = 1 to 20 do
+		s1 = save[1]
+		s2 = save[2]
+		s1[1] = s1[1] -- deep copy s1
+		s2[1] = s2[1] -- deep copy s2
+		timing_data = append(timing_data, call_func(r,{}))
+	end for
+	sequence stats = average(timing_data) & 2*stdev(timing_data)
+	sequence prefix = get_metric_prefix(stats[1])
+	stats /= prefix[1]
+	printf(1, "%s: %.3f +- %f %ss\n", prepend(append(stats,prefix[2]),message) )
+end procedure
 
-function sum_trial_1()
+display_report( "Time to add an integer to a 5000 long sequence of integers", "sum_trial_is")
+
+function sum_trial_s2()
 	atom ti 
 	ti = time()
 	while ti = time() do
@@ -55,16 +65,10 @@ function sum_trial_1()
 	return (tf-ti)/inner_trials
 end function
 
-timing_data = {}
-for i = 1 to 20 do
-	timing_data = append(timing_data, sum_trial_1())
-end for
-stats = average(timing_data) & 2*stdev(timing_data)
-prefix = get_metric_prefix(stats[1])
-stats /= prefix[1]
-printf(1, "Time to add two 5000 long sequences of integers: %.3f +- %f %ss\n", append(stats,prefix[2]) )
 
-function subtract_trial_1()
+display_report( "Time to add two 5000 long sequences of integers", "sum_trial_s2")
+
+function subtract_trial_si()
 	atom ti 
 	ti = time()
 	while ti = time() do
@@ -76,15 +80,8 @@ function subtract_trial_1()
 	atom tf = time()
 	return (tf-ti)/inner_trials
 end function
+display_report( "Time to subtract an integer from a 5000 long sequence of integers", "subtract_trial_si")
 
-timing_data = {}
-for i = 1 to 20 do
-	timing_data = append(timing_data, subtract_trial_1())
-end for
-stats = average(timing_data) & 2*stdev(timing_data)
-prefix = get_metric_prefix(stats[1])
-stats /= prefix[1]
-printf(1, "Time to subtract an integer from a 5000 long sequence of integers: %.3f +- %f %ss\n", append(stats,prefix[2]) )
 
 function subtract_trial_2()
 	atom ti 
@@ -99,13 +96,9 @@ function subtract_trial_2()
 	return (tf-ti)/inner_trials
 end function
 
-timing_data = {}
-for i = 1 to 20 do
-	timing_data = append(timing_data, subtract_trial_1())
-end for
-stats = average(timing_data) & 2*stdev(timing_data)
-prefix = get_metric_prefix(stats[1])
-stats /= prefix[1]
-printf(1, "Time to subtract an a 5000 long sequence of integers from another 5000 long sequence of integers: %.3f +- %f %ss\n", append(stats,prefix[2]) )
+
+display_report( "Time to subtract an a 5000 long sequence of integers from\n" &
+	"another 5000 long sequence of integers", 
+	"subtract_trial_2")
 
 maybe_any_key("Press any key to Close")
