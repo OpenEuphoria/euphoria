da6ff392d30c4b18ebe35c118cd4019c9a8a8d00
Matt Lewis <matthewwalkerlewis@gmail.com>
2011-04-27 19:46:27 -0400

    * use cross processor type manifest file

--- source/configure.bat
+++ source/configure.bat
@@ -268,12 +268,6 @@ echo TRUNKDIR=%TRUNKDIR% >> config.wat
 echo BUILDDIR=%FULL_BUILDDIR% >> config.wat
 
 rem ============================================================
-rem Copy 32-bit manifest file
-rem ============================================================
-
-copy eu.manifest32 eu.manifest
-
-rem ============================================================
 rem Copy temporary .wat includes
 rem ============================================================
 
