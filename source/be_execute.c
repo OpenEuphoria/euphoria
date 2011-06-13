f2a7080e78df05573fffe25e5e224ed583fc4e05
Matt Lewis <matthewwalkerlewis@gmail.com>
2011-06-13 09:20:49 -0400

    * Fixed machine crash in [[:splice]] when splicing an atom before beginning of sequence or after end * fixes ticket 675

--- source/be_execute.c
+++ source/be_execute.c
@@ -4069,7 +4069,7 @@ void do_exec(int *start_pc)
 
 			case L_SPLICE:
 			deprintf("case L_SPLICE:");
-				splins = 1;
+				splins = !IS_ATOM( *(object_ptr) pc[2] );
 				goto spin;
 			case L_INSERT:
 			deprintf("case L_INSERT:");
