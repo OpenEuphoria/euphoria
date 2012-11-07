995deb58134ecd3b1a8fec72fb1b10e68d8dbff5
Shawn Pringle <shawn.pringle@gmail.com>
2012-11-07 20:33:14 -0300

    * made C code clearer * ticket 818

--- source/test818.c
+++ source/test818.c
@@ -1,5 +1,7 @@
 #include "euphoria.h"
+#define EUPHORIA_MIN_INT MININT
 #include <string.h>
+
 #ifdef __WIN32
 #define EXPORT __declspec(dllexport)
 #else
@@ -7,9 +9,8 @@
 #endif
 
 /* The expression falls within the signed int range */
-#define MAKE_FN(ctype,etype) EXPORT ctype etype ## _faux_sequence() { return MININT - 20; }
+#define MAKE_FN(ctype,etype) EXPORT ctype etype ## _faux_sequence() { return EUPHORIA_MIN_INT - 20; }
 
-/* Which of these are not 64-bit on 64-bit platforms? */
 MAKE_FN(int,C_INT)
 MAKE_FN(long,C_LONG)
 MAKE_FN(long long,C_LONGLONG)
