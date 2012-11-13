39ab1d17aa3e9f022ebe10056bae9f5f743712a8
Shawn Pringle <shawn.pringle@gmail.com>
2012-11-13 09:18:34 -0300

    * made MAKE_BORDER_FUNCTIONS more readble * created MAKE_ID_FUNCTION macro. * created id functions * created bit_repeat function in test818.c * this is for a bad commit message changeset * ticket 818

--- source/test818.c
+++ source/test818.c
@@ -1,3 +1,10 @@
+/**
+ * Trivial functions for testing how EUPHORIA's EUPHORIA to C bridge c_func and c_proc 
+ * convert the values from EUPHORIA to C.
+ * 
+ * Note: Attemps to use bool and C_BOOL in any of these macros result in an error.  There is 
+ * something special about one of these identifiers...  */ 
+
 #include "euphoria.h"
 /* smallest EUPHORIA integer */
 #define EUPHORIA_MIN_INT MININT
@@ -18,25 +25,57 @@
 #endif
 #endif
 
+/* predefined as some macro */
+#undef bool
+typedef enum {false,true} bool;
 #define BFFD_SHORT_VALUE (0xC * (1 << (8*sizeof(short)-8)))
 
 /* The expression falls within the signed int range but outside of that of EUPHORIA */
 #define MAKE_BORDER_FUNCTIONS(ctype,etype) \
 	EXPORT ctype etype ## _below_EUPHORIA_MIN_INT() \
-	{ return EUPHORIA_MIN_INT - 20; }\
+	{\
+		return EUPHORIA_MIN_INT - 20;\
+	}\
 	EXPORT ctype etype ## _above_EUPHORIA_MAX_INT() \
-	{ return EUPHORIA_MAX_INT + 20; }\
+	{\
+		return EUPHORIA_MAX_INT + 20;\
+	}\
 	EXPORT ctype etype ## _NOVALUE() \
-	{ return NOVALUE; }\
+	{\
+		return NOVALUE; \
+	}\
 	EXPORT ctype etype ## _half_MIN() \
-	{ return EUPHORIA_MIN_INT/2; }\
+	{\
+		return EUPHORIA_MIN_INT/2;\
+	}\
 	EXPORT ctype etype ## _half_MAX() \
-	{ return EUPHORIA_MAX_INT/2; }
-
+	{\
+		return EUPHORIA_MAX_INT/2; \
+	}
+	
 MAKE_BORDER_FUNCTIONS(int,C_INT)
 MAKE_BORDER_FUNCTIONS(long,C_LONG)
 MAKE_BORDER_FUNCTIONS(long long,C_LONGLONG)
 
+#define MAKE_ID_FUNCTION(ctype,etype) \
+	EXPORT ctype etype ## _id(ctype val) {\
+		return val;\
+	}
+
+MAKE_ID_FUNCTION(signed char, C_CHAR)
+MAKE_ID_FUNCTION(unsigned char, C_UCHAR)
+MAKE_ID_FUNCTION(short, C_SHORT)
+MAKE_ID_FUNCTION(unsigned short, C_USHORT)
+MAKE_ID_FUNCTION(int, C_INT)
+MAKE_ID_FUNCTION(unsigned int, C_UINT)
+MAKE_ID_FUNCTION(long, C_LONG)
+MAKE_ID_FUNCTION(unsigned long, C_ULONG)
+MAKE_ID_FUNCTION(long long, C_LONGLONG)
+
+MAKE_ID_FUNCTION(float, C_FLOAT)
+MAKE_ID_FUNCTION(double, C_DOUBLE)
+
+	
 #define MAKE_NEAR_HASHC_FN(ctype,etype,min) \
 	ctype etype ## _BFFD_value = min - 20; \
 	EXPORT ctype etype ## _BFF_FD() { return etype ## _BFFD_value; }
@@ -46,3 +85,16 @@ MAKE_NEAR_HASHC_FN(short,     C_SHORT,    BFFD_SHORT_VALUE)
 MAKE_NEAR_HASHC_FN(int,       C_INT,      BFFD_INT_VALUE)
 MAKE_NEAR_HASHC_FN(long,      C_LONG,     BFFD_LONG_VALUE)
 MAKE_NEAR_HASHC_FN(long long, C_LONGLONG, BFFD_LONGLONG_VALUE)
+
+
+unsigned long long bit_repeat(bool bit, unsigned char count) {
+	long long bit_vector = 0LL;
+	// make sure bit is boolean.
+	bit = (bit != false);
+	while (count--) {
+		bit_vector <<= 1;
+		bit_vector |= bit;
+	}
+	return bit_vector;
+}
+
