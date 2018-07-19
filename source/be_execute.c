428ad2be620fd3e6ca95d0ddb66bf9c09f61f226
Shawn David Pringle B.Sc <shawn.pringle@gmail.com>
2018-07-19 17:43:32 -0300

    * The change can sometimes help developers find bugs when they forget to sanitize parameters before calling some floating point routines from the C library.

--- source/be_execute.c
+++ source/be_execute.c
@@ -37,6 +37,7 @@
 /******************/
 /* Included files */
 /******************/
+#define _SVID_SOURCE
 #include <stdio.h>
 #include <time.h>
 #ifdef EUNIX
@@ -756,6 +757,12 @@ void InitExecute()
 	// a bit of cleanup - tick rate, profile, active page etc.
 #endif
 
+// detect matherr support
+#if defined(DOMAIN) && defined(SING) && defined(OVERFLOW) && defined(UNDERFLOW) && defined(TLOSS) && defined(PLOSS)
+	// enable our matherr function
+	_LIB_VERSION = _SVID_;
+#endif
+    
 #ifdef EWINDOWS
 		/* Prevent "Send Error Report to Microsoft dialog from coming up
 		   if this thing has an unhandled exception.  */
